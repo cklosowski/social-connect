99234b7df7e703c0c1a5207626cbe9e5fe5e3f98
Chris Klosowski <cklosowski@gmail.com>
2014-07-02 06:48:40 -0700

    Set the url on first view

--- social-connect.php
+++ social-connect.php
@@ -342,6 +342,7 @@ function sc_filter_avatar($avatar, $id_or_email, $size, $default, $alt) {
 					$avatar_data = json_decode( wp_remote_retrieve_body( wp_remote_get( 'http://graph.facebook.com/' . $social_id . '/picture?redirect=false&type=square&height=200&width=200' ) ), true );
 					if ( isset( $avatar_data['data'] ) && !empty( $avatar_data['data']['url'] ) ) {
 						update_user_meta( $user_id, '_social_connect_avatar_url', $avatar_data['data']['url'] );
+						$custom_avatar = $avatar_data['data']['url'];
 					} else {
 						$custom_avatar = "http://graph.facebook.com/$social_id/picture?type=$size_label";
 					}
